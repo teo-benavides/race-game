9daff9a9e88282002f0631e1493816abd255d99a
Radagast <fernandobaroli@Radagasts-MacBook-Air.local>
2026-05-02 01:22:21 -0300

    VFXs varios

--- scenes/player.gd
+++ scenes/player.gd
@@ -13,46 +13,46 @@ var invincible := false
 var flash_cooldown: float = 0.0
 var _engine_power: float = 0.0
 var engine_power: float:
-    set(value):
-        _engine_power = value
-        $EngineSFX.pitch_scale = lerpf(MIN_ENGINE_SFX_PITCH, MAX_ENGINE_SFX_PITCH, _engine_power)
-    get():
-        return _engine_power
+	set(value):
+		_engine_power = value
+		$EngineSFX.pitch_scale = lerpf(MIN_ENGINE_SFX_PITCH, MAX_ENGINE_SFX_PITCH, _engine_power)
+	get():
+		return _engine_power
 
 func _physics_process(delta: float) -> void:
-    if invincible and flash_cooldown <= 0.0:
-        %Model.visible = !%Model.visible
-    
-    flash_cooldown -= FLASH_INTERVAL * delta
+	if invincible and flash_cooldown <= 0.0:
+		%Model.visible = !%Model.visible
+	
+	flash_cooldown -= FLASH_INTERVAL * delta
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-    if body is Goal:
-        $EngineSFX.stop()
-        finished_track.emit()
-    elif body.name == "PassbyBody":
-        #$PassbySFX.play()
-        pass
-    else:
-        $DeathSFX.play()
-        $Explosion00/Sparks.emitting = true
-        $Explosion00/Flash.emitting = true
-        $Explosion00/Burn.emitting = true
-        $Explosion00/SmokeUranium.emitting = true
-        %Model.visible = false
-        $Area3D.set_deferred("monitoring", false)
-        $RespawnTimer.start()
-        died.emit()
+	if body is Goal:
+		$EngineSFX.stop()
+		finished_track.emit()
+	elif body.name == "PassbyBody":
+		#$PassbySFX.play()
+		pass
+	else:
+		$DeathSFX.play()
+		$Explosion00/Sparks.emitting = true
+		$Explosion00/Flash.emitting = true
+		$Explosion00/Burn.emitting = true
+		$Explosion00/SmokeUranium.emitting = true
+		%Model.visible = false
+		$Area3D.set_deferred("monitoring", false)
+		$RespawnTimer.start()
+		died.emit()
 
 func _on_respawn_timer_timeout() -> void:
-    invincible = true
-    $InvincibilityTimer.start()
-    respawned.emit()
+	invincible = true
+	$InvincibilityTimer.start()
+	respawned.emit()
 
 func _on_invincibility_timer_timeout() -> void:
-    invincible = false
-    %Model.visible = true
-    $Area3D.set_deferred("monitoring", true)
+	invincible = false
+	%Model.visible = true
+	$Area3D.set_deferred("monitoring", true)
 
 func _on_boost_area_body_entered(_body: Node3D) -> void:
-    boosted.emit()
-    $BoostSFX.play()
+	boosted.emit()
+	$BoostSFX.play()
